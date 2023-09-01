90a59f7d32a3d0ff5d45885bf688ac8e81ca9f9d
Queen <queenumesi01@gmail.com>
2023-09-01 07:45:19 +0100

    build: create navbar and add responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,18 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 
 import "./App.css";
-import Header from "./components/Navbar";
-import Footer from "./components/Footer";
+import Header from "./components/Header/Navbar";
+import Footer from "./components/Footer/Footer";
 
 const App = () => {
   return (
-    <>
+    <div className=" font-DM_Sans bg-slate-400 text-global-color-5 ">
       <Header />
       <main>
         <Outlet />
       </main>
       <Footer />
-    </>
+    </div>
   );
 };
 
---
+++ src/components/Header/Navbar.jsx
@@ -0,0 +1,92 @@
+import React, { useState, useRef } from "react";
+import { NavLink, Link } from "react-router-dom";
+import Badge from "@mui/material/Badge";
+import IconButton from "@mui/material/IconButton";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { Turn as Hamburger } from "hamburger-react";
+import classNames from "classnames";
+
+import "./Navbar.css";
+import { Logo } from "../../assets/icons/index";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      light: "#757ce8",
+      main: "#fff",
+      dark: "#002884",
+      contrastText: "#000",
+    },
+  },
+});
+
+// active link function
+const active = ({ isActive, isPending }) =>
+  isPending ? "pending" : isActive ? "active" : "";
+
+const Header = () => {
+  const menuRef = useRef();
+  const [isOpen, setOpen] = useState(false);
+
+  return (
+    <header className="relative">
+      <nav className="flex justify-between items-center px-8 min-h-[90px] ">
+        <Link to="/">
+          <img src={Logo} srcSet="" alt="cycle shop" width={80} height={48} />
+        </Link>
+
+        {/* MENU LINKS */}
+        <ul
+          ref={menuRef}
+          className={classNames(
+            "uppercase font-semibold md:flex gap-5",
+            !isOpen && window.innerWidth <= 768 ? "hidden" : ""
+          )}
+        >
+          {[
+            ["Home", "/"],
+            ["Bicycles", "product-category/bicycles"],
+            ["Accessories", "product-category/accessories"],
+            ["About Us", "about-us"],
+            ["Contact", "contact"],
+          ].map(([title, url], index) => (
+            <li key={index}>
+              <NavLink
+                to={url}
+                className={`hover:text-global-color-7 ${active}`}
+              >
+                {title}
+              </NavLink>
+            </li>
+          ))}
+        </ul>
+
+        <div className="flex items-center gap-1">
+          <span className=" cart-total font-black ">$0.00</span>
+
+          <ThemeProvider theme={theme}>
+            <IconButton aria-label="cart" color="primary">
+              <Badge badgeContent={0} color="primary" showZero>
+                <ShoppingCartIcon color="primary" />
+              </Badge>
+            </IconButton>
+          </ThemeProvider>
+
+          <div className="md:hidden">
+            <Hamburger
+              label="Show menu"
+              toggled={isOpen}
+              toggle={setOpen}
+              rounded
+              duration={0.8}
+              size={24}
+            />
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;

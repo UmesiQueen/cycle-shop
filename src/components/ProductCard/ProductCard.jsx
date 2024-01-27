99eb9d85073acc461cb4dcb91e85fbdafa905e9a
Queen <queenumesi01@gmail.com>
2024-01-27 21:03:27 +0100

    feat: add single product to cart on cart-icon click

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -1,12 +1,15 @@
-import { useState, useEffect } from "react";
+import { useState, useContext } from "react";
 import { Link } from "react-router-dom";
 import Icon from "@mdi/react";
 import { mdiCart, mdiStarOutline } from "@mdi/js";
+import classNames from "classnames";
 
 import "./style.css";
+import { CartItemsContext } from "../../hooks/CartContext";
 
 const ProductCard = ({
   productType,
+  productId,
   name,
   src,
   price,
@@ -14,23 +17,67 @@ const ProductCard = ({
   srcSet,
   prices,
 }) => {
-  const [isActive, setActive] = useState("");
-  const [productName, setProductName] = useState("");
+  const { setNewOrder, addToCart } = useContext(CartItemsContext);
+  const [isActive, setActive] = useState(-1);
+  const [inActive, setInActive] = useState([]);
 
-  useEffect(() => {
-    const regex = new RegExp("^[a-zA-Z0-9]*$");
-    const formatString = name
-      .split(" ")
-      .filter((item) => item.match(regex))
-      .join("-")
-      .toLowerCase();
+  const regex = new RegExp("^[a-zA-Z0-9]*$");
+  const productName = name
+    .split(" ")
+    .filter((item) => item.match(regex))
+    .join("-")
+    .toLowerCase();
 
-    setProductName(formatString);
-  }, [name]);
+  const handleOnClick = (e) => {
+    if (productType === "Accessories") {
+      if (isActive < 0) {
+        const element = e.currentTarget;
+        element.classList.add("shake");
+        setInActive([0]);
+        const nextInActiveIndex = setInterval(() => {
+          element.classList.remove("shake");
+          setInActive((prev) => {
+            return [...prev, prev.slice(-1)[0] + 1];
+          });
+        }, 450);
+
+        let delayTime = (prices.length - 1) * 1000;
+
+        setTimeout(() => {
+          clearInterval(nextInActiveIndex);
+          setInActive([]);
+        }, delayTime);
+
+        return false;
+      }
+    }
+
+    return addToCart();
+  };
+
+  let defaultOrderState = {
+    productId,
+    type: productType,
+    quantity: 1,
+    cost: price,
+  };
+
+  const setDefaultOrderState = () => {
+    if (productType === "Accessories") {
+      if (isActive !== -1) {
+        const { cost, size } = prices.filter(
+          (item, index) => isActive === index
+        )[0];
+
+        return setNewOrder({ ...defaultOrderState, cost, size });
+      }
+    }
+    return setNewOrder(defaultOrderState);
+  };
 
   return (
     <div className=" w-fit h-full flex flex-col">
-      <div className="relative mb-3">
+      <div className="relative mb-3" onMouseEnter={setDefaultOrderState}>
         <Link to={`/product/${productName}`} className="product-image">
           <img
             src={src}
@@ -41,14 +88,9 @@ const ProductCard = ({
             height={800}
           />
         </Link>
-        <a
-          href="/add-to-cart"
-          className="cart "
-          title="add to cart"
-          data-quantity={1}
-        >
+        <button className="cart" title="add to cart" onClick={handleOnClick}>
           <Icon path={mdiCart} size={0.8} />
-        </a>
+        </button>
       </div>
 
       <span className="text-sm text-global-color-3">{productType}</span>
@@ -69,7 +111,7 @@ const ProductCard = ({
           </span>
         ) : productType === "Accessories" ? (
           <>
-            {isActive.length < 1 ? (
+            {isActive < 0 ? (
               <span className="font-extrabold text-global-color-3 py-2 ">
                 ${Math.min(...prices.map((item) => item.cost))} - $
                 {Math.max(...prices.map((item) => item.cost))}
@@ -88,8 +130,13 @@ const ProductCard = ({
                   <li
                     key={index}
                     role="button"
-                    className={isActive === index ? "active" : ""}
-                    onClick={() => setActive(index)}
+                    className={classNames(
+                      isActive === index && "active",
+                      inActive.includes(index) ? "inactive" : ""
+                    )}
+                    onClick={() => {
+                      setActive(index);
+                    }}
                   >
                     {item.size}
                   </li>
